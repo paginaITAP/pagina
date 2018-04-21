93f2bd46387764a5c06f24ee6886a58d8f28db16
Ikki <figueroajesus2015@hotmail.com>
2018-04-21 09:23:42 -0500

    Elimine teachers de nuestro proyecto

--- app/controllers/teachers_controller.rb
+++
@@ -1,3 +0,0 @@
-class TeachersController < ApplicationController
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
   resources :photos
   resources :videos
 
-  resources :teachers
   resources :managers
 
   resources :news
